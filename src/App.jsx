237213614ba00b8e796d46e98ede8989762b0c90
ccogv\brittaneyharkness <Brittaney.Harkness@cookcountyil.gov>
2024-01-25 10:56:56 -0600

    button group alignment

--- src/App.jsx
+++ src/App.jsx
@@ -26,10 +26,10 @@ function App() {
         <Stack id="main-stack" direction="row" gap={1} justifyContent="space-between" padding={0} height="100%">
           <LeftPanel/>
           <Box flexDirection="column" flex={4} padding={0} display="flex" alignItems="center">
-              <Box flexDirection="column" alignItems="left" width="100%" display={{xs:'none', sm:'none', md:'flex'}}>
+              <Box flexDirection="column" alignItems="left" width="100%" display={{xs:'none', sm:'flex', md:'flex'}}>
                 <MapButtonGroup/>
               </Box>
-              <Box flexDirection="column" alignItems="center" width="100%" display={{xs:'flex', sm:'flex', md:'none'}}>
+              <Box flexDirection="column" alignItems="center" width="100%" display={{xs:'flex', sm:'none', md:'none'}}>
                 <MapButtonGroup/>
               </Box>
               
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -14,16 +14,17 @@ const NavBar = () => {
             <AppBar position="sticky">
                 <StyledToolbar>
                     <Stack direction={{xs:'column', sm:'row'}} justifyContent="space-between" alignItems="center" width='100%' gap={2}>
-                        <Stack direction="row" alignItems='center' flex={1} width="auto">
                             <IconButton
                                 size="large"
                                 edge="start"
                                 color="inherit"
                                 aria-label="menu"
-                                sx={{ mr: 2, display:{xs: 'block', sm: 'none'}, position:"absolute"}}
+                                sx={{ mr: 2, display:{xs: 'block', sm: 'none'}, left:10, top:0, position:"absolute"}}
                             >
-                                <MenuIcon />
+                                <MenuIcon fontSize="large"/>
                             </IconButton>
+                        <Stack direction="row" alignItems='center' flex={1} width="auto">
+                            
                
                             <Stack direction="row" alignContent="center" gap={1} height='100%' width="auto">
                                 <Avatar sx={{bgcolor:"orange"}}/>    
