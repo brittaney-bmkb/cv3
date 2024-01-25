cdbf3f9f40f6be293a77c16565bf29c4f750f9da
ccogv\brittaneyharkness <Brittaney.Harkness@cookcountyil.gov>
2024-01-25 10:26:02 -0600

    map button group component added

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import { useState } from 'react'
 import './App.css'
 import NavBar from './components/NavBar'
 import WebMapView from './components/WebMapView'
-import { Box, Stack, Grid, ThemeProvider, createTheme } from '@mui/material'
+import { Box, Stack, Grid, ThemeProvider, createTheme, Button } from '@mui/material'
 import Panel, { BottomPanel, LeftPanel, RightPanel } from './components/Panel'
 import ToggleButton from './components/ToggleButton'
 import '@esri/calcite-components/dist/calcite/calcite.css';
+import MapButtonGroup from './components/MapButtonGroup'
 
 const theme = createTheme({
   palette:{
@@ -22,9 +23,10 @@ function App() {
   return (
       <Box display="flex" flexDirection="column" height="100vh">
           <NavBar/>
-        <Stack id="main-stack" direction="row" gap={2} justifyContent="space-between" padding={0} height="100%">
+        <Stack id="main-stack" direction="row" gap={1} justifyContent="space-between" padding={0} height="100%">
           <LeftPanel/>
-          <Box flexDirection="column" flex={4} padding={0} display="flex">
+          <Box flexDirection="column" flex={4} padding={0} display="flex" alignItems="center">
+            <MapButtonGroup/>
             <WebMapView/>
             <BottomPanel/>
           </Box>
---
+++ src/components/MapButtonGroup.jsx
@@ -0,0 +1,15 @@
+import { Box, Button, Stack } from "@mui/material"
+import { theme } from "../theme"
+
+const MapButtonGroup = () => {
+    return(
+        <Stack direction="row" position="absolute" spacing={2} padding={2}>
+            <Button variant="contained" sx={{bgcolor:theme.palette.primary}}>Button</Button>
+            <Button variant="contained" sx={{bgcolor:theme.palette.primary}}>Button</Button>
+            <Button variant="contained" sx={{bgcolor:theme.palette.primary}}>Button</Button>
+            <Button variant="contained" sx={{bgcolor:theme.palette.primary}}>Button</Button>
+        </Stack>
+    )
+}
+
+export default MapButtonGroup
