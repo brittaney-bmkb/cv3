415ca3c548e1b780bde8d6098c59eb06dad43858
Sigfrido Gomez (Geographic Information Systems) <Sigfrido.Gomez@cookcountyil.gov>
2024-02-28 11:55:01 -0600

    adding default ESRI measure widget, keeping it simple.

--- src/components/Widgets/MeasureWidget.jsx
+++ src/components/Widgets/MeasureWidget.jsx
@@ -18,10 +18,13 @@ import NativeSelect from '@mui/material/NativeSelect';
 
 import SquareFootIcon from '@mui/icons-material/SquareFoot';
 import StyledButtonFilledPrimary, { StyledPanelButton } from "../Button/Button";
-import { initializeMeasureWidget } from "../../arcgis/widgets/measurement";
+
 
 import Measurement from "@arcgis/core/widgets/Measurement.js";
 import { view } from "../../arcgis/webmap/webmap";
+
+
+// import { initializeMeasureWidget } from "../../arcgis/widgets/measurement";
 
 
 // this lifted from comparable property search and will needed to be updated for this widget
@@ -34,92 +37,76 @@ const MeasureWidget = () => {
     const [ measureValue, setMeasureValue ] = useState(0)
     const [activeTool, setActiveTool] = useState('distance')
 
-    const [clearButton, setClearButton] = useState(null)
+    // const [clearButton, setClearButton] = useState(null)
 
     useEffect(() => {
-
         const createMeasureWidget = () => {
             if(measureDiv.current){
                 if(!measureWidget.current){
-                    console.log('measureWidget.current ', measureWidget)
                     measureWidget.current = new Measurement({
                         areaUnit: "square-us-feet",
                         linearUnit: "feet",
                         container: measureDiv.current,
-                        view: view
+                        view: view,
+                        activeTool: null
                     })
                     setMeasureWidgetState('measuring')
+                    // measureWidget.current.renderNow()
                 }
             }        
         }
         createMeasureWidget();
-    }, [measureWidget, measureDiv])
 
-    useEffect(() => {
+        view.ui.add(measureWidget.current, 'MEASURECONTAINER');
 
-        const updateActiveTool = () =>{
-            if(measureWidget.current && measureWidgetState === 'measuring'){
-                measureWidget.current.activeTool = activeTool
+        return () => {
+            // Cleanup function to destroy the measure widget when unmounting
+            if (!measureWidget.current) {
+                measureWidget.current.destroy();
+                measureWidget.current = null;
             }
+        };
+
+    // }, [measureWidget, measureDiv])
+    // }, [])
+    }, [measureWidget, measureDiv, activeTool, view]);
+
+
+
+    const handleToolChange = (tool) => {
+        if (measureWidget.current) {
+            measureWidget.current.activeTool = tool;
+            setActiveTool(tool);
         }
-        updateActiveTool()
+    };
 
-    }, [activeTool, measureWidgetState])
 
-    const newMeasurement = (event) => {
-        // let formattedText  =  event.target.textContent.toLowerCase()
-        // setActiveTool(formattedText) 
-        
-        measureWidget.current.startMeasurement()
-        setMeasureWidgetState("measuring")
-    }
-
-    const clearMeasureTool = () => {
-        // setActiveTool(null) 
-        measureWidget.current.clear()
-        setMeasureWidgetState(null) 
-    }
-
-    const updateActiveTool = (event) => {
-        let formattedText  =  event.target.textContent.toLowerCase()
-        setActiveTool(formattedText) 
-    }
-    
     return (
         <div id="MEASURECONTAINER" style={{width: '100%', height: '100%'}} >
-            <Stack direction="row" gap={2} display={{xs:'none', sm:'flex', md:'flex', lg:'flex' }}> 
-                <StyledPanelButton
-                    text1={'Distance'} text2={'Area'} 
-                    text3={'Location'} 
-                    icon1={<StraightenOutlinedIcon/>} 
-                    icon2={<SquareFootOutlinedIcon/>} 
-                    icon3={<MyLocationOutlinedIcon/>}
-                    onClick={updateActiveTool} 
-                />
+
+            <Stack direction="row" gap={2}>
+                <StyledButtonFilledPrimary
+                    variant={activeTool === 'distance' ? 'contained' : 'outlined'}
+                    color="primary"
+                    startIcon={<StraightenOutlinedIcon/>}
+                    text={'Distance'}
+                    onClick={() => handleToolChange('distance')}
+                /> 
+
+                <StyledButtonFilledPrimary
+                    variant={activeTool === 'area' ? 'contained' : 'outlined'}
+                    color="primary"
+                    startIcon={<SquareFootOutlinedIcon/>}
+                    text={'Area'}
+                    onClick={() => handleToolChange('area')}
+                /> 
+
             </Stack>
+
             <Box display="flex" flexDirection="column" alignItems="center" rowGap={1} p={1}> 
                 <Box ref={measureDiv}> </Box>
-                {measureWidget.current && measureWidgetState === 'measuring' ? 
-                    <Box >
-                        <StyledButtonFilledPrimary
-                            variant="contained"
-                            color="primary"
-                            startIcon={<ReplayIcon/>}
-                            text={'Clear'}
-                            onClick={clearMeasureTool}
-                        /> 
-                    </Box>
-                    :
-                    <Box >
-                        <Box sx={{ m: 2  }} >Select a new measurement button.</Box>
-                        <StyledButtonFilledPrimary 
-                            variant="contained"
-                            color="primary"
-                            text={'New Measurement'}
-                            onClick={newMeasurement}
-                        />  
-                    </Box>
-                } 
+
+
             </Box>
         </div>
             )      
