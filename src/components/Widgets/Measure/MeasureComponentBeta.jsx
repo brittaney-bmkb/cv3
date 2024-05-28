5be9422711eadc3c3c66bf8f23bb1b8e09450a1e
Sigfrido Gomez (Geographic Information Systems) <Sigfrido.Gomez@cookcountyil.gov>
2024-05-28 13:52:23 -0500

    working on converting polyline to polygon, almost there. cleaning up code after commit.

--- src/components/Widgets/Measure/MeasureComponentBeta.jsx
+++ src/components/Widgets/Measure/MeasureComponentBeta.jsx
@@ -10,8 +10,21 @@ import SketchViewModel from "@arcgis/core/widgets/Sketch/SketchViewModel.js";
 import Sketch from "@arcgis/core/widgets/Sketch.js";
 
 
-// https://developers.arcgis.com/javascript/latest/tutorials/find-length-and-area/
-
+//// https://developers.arcgis.com/javascript/latest/tutorials/find-length-and-area/
+//// https://developers.arcgis.com/javascript/latest/tutorials/find-length-and-area/#add-an-event-listener
+//// https://developers.arcgis.com/javascript/latest/api-reference/esri-widgets-Sketch.html#update
+//// https://developers.arcgis.com/javascript/latest/api-reference/esri-widgets-Sketch.html#event-update
+//// https://developers.arcgis.com/javascript/latest/api-reference/esri-geometry-geometryEngine.html
+//// https://developers.arcgis.com/javascript/latest/api-reference/esri-widgets-Sketch.html#properties-summary
+//// https://developers.arcgis.com/javascript/latest/sample-code/sketch-geometries/
+//// https://developers.arcgis.com/javascript/latest/api-reference/esri-views-interactive-snapping-SnappingOptions.html
+//// https://developers.arcgis.com/javascript/latest/api-reference/esri-widgets-Sketch.html
+//// https://community.esri.com/t5/arcgis-javascript-maps-sdk-questions/how-to-convert-a-line-to-polygon-in-js-api-4-x/m-p/420334#M38612
+//// 
+//// 
+//// 
+//// 
+//// 
 
 
 // this lifted from comparable property search and will needed to be updated for this widget
@@ -20,62 +33,106 @@ const MeasureComponentBeta = () => {
     const { mapView, map } = UseAppContext()
     const sketchRef = useRef(null)
 
-
-    // const getLength = (line) =>{
-    //     const geodesicLength = geometryEngine.geodesicLength(line, "kilometers");
-    //     const planarLength = geometryEngine.planarLength(line, "kilometers");
-
-    //     console.log('geodesicLength: ', geodesicLength);
-    //     console.log('planarLength: ', planarLength);
-    //     console.log();
-
-    //     // return(geodesicLength, planarLength)
-    // }
-
-    function getArea(polygon) {
-        // const geodesicArea = geometryEngine.geodesicArea(polygon, "square-kilometers");
+    const getArea = (polygon) => {
+        // TODO make this a state to update in REACT
         const planarArea = geometryEngine.planarArea(polygon, "square-kilometers");
-        // console.log('geodesicArea:', geodesicArea.toFixed(2), 'planarArea:', planarArea.toFixed(2))
         console.log('planarArea:', planarArea.toFixed(2))
-        // return geodesicArea, planarArea
-
     }
 
-    function getLength(line) {
-        // const geodesicLength = geometryEngine.geodesicLength(line, "kilometers");
+    const getLength= (line) =>{
         const planarLength = geometryEngine.planarLength(line, "kilometers");
-        // console.log('geodesicLength:', geodesicLength.toFixed(2), 'planarLength:', planarLength.toFixed(2))
         console.log('planarLength:', planarLength.toFixed(2))
-    }    
+    }
 
     function switchType(geom) {
         switch (geom.type) {
             case "polygon":
                 getArea(geom);
-                
                 break;
             case "polyline":
                 getLength(geom);
-                
                 break;
             default:
                 console.log("No value found");
         }
-    }       
+    }
+
+    const checkLatLongArray = (geom) => {
+        let polygonRings = geom.paths[0]
+        let firstLat =polygonRings[0][0]
+        let firstLon = polygonRings[0][1]
+        let lastLat = polygonRings[polygonRings.length -1][0]
+        let lastLon = polygonRings[polygonRings.length -1][1]
+
+        let results = (firstLat === lastLat && firstLon === lastLon)
+
+        return results
+
+        // return [firstLat === lastLat, firstLon === lastLon ]
+
+    }
+
+    const convertPolyline2Polygon = (geom) =>{
+
+        let polygon = {
+            type:"polygon",
+            paths:[]
+        };
+
+        console.log('Checking the conver geom',geom.paths[0])
+
+
+        switch (geom.type) {
+            // case "polygon":
+            //     getArea(geom);
+            //     break;
+            case "polyline":
+
+            
+                // polygon.rings = geom.graphic.geometry.paths[0]
+                polygon.rings = geom.paths[0]
+
+                let results = checkLatLongArray(geom)
+                console.log('Checking if both geoms are positive: ',results);
+
+
+                // getLength(geom);
+                // break;
+            default:
+                console.log("No value found");
+        }
+
+    }
+
+
+
+
+    function switchType(geom) {
+        switch (geom.type) {
+            case "polygon":
+                getArea(geom);
+                break;
+            case "polyline":
+                getLength(geom);
+                break;
+            default:
+                console.log("No value found");
+        }
+    }    
 
     useEffect(() => {
+
         let graphicsLayer = new GraphicsLayer()
-
-
         if (map){
             //event handler opn the sketch 
             map.add(graphicsLayer)
             console.log("checking map ", map)
         }
-        console.log('sketchRef.current')
-        console.log(sketchRef.current)
+        // console.log('sketchRef.current')
+        // console.log(sketchRef.current)
+
         if (sketchRef.current){
-            console.log('checking current')
+            // console.log('checking current')
             const sketch = new Sketch({
                 view:mapView,
                 layer: graphicsLayer, 
@@ -93,51 +150,76 @@ const MeasureComponentBeta = () => {
                     },
                     settingsMenu: true,
                     undoRedoMenu: true
-                  },
-
+                },
                 snappingOptions: { // autocasts to SnappingOptions()
                     enabled: true, // global snapping is turned on
                     // assigns a collection of FeatureSnappingLayerSource() and enables feature snapping on this layer
                     featureSources: [{ layer: graphicsLayer, enabled: true }]
-                  }                
+                }                
             })
-            // console.log('Checking initilization of sketch', sketchRef.current)
-            // sketch.on("create", (event) => {
-            //     console.log("new sketch event", event)
-            //     // console.log('checking on sketch propperties');
-            //     // console.log(event.graphic.geometry.spatialReference.metersPerUnit)
-            //     console.log('checking on sketch propperties');
-            //     console.log(event.graphic.geometry)
-            // }
-            // )
 
-            sketch.on("update", (e) => {
+            sketch.on("create", (e) => {
+                console.log('create sketch', e.state);
+                console.log("checking graphics geom", e.graphic.geometry.paths[0])
+
                 // console.log("sketch on update", e.graphics[0].geometry)
-                
                 // const geometry = e.graphic.geometry;
-                const geometry =  e.graphics[0].geometry;
-                if (e.state === "start") {
-                    console.log("sketch on start",  geometry)
+
+
+                
+                if (e.state === "active") {
+                    const geometry =  e.graphic.geometry;
+                    console.log("sketch on active",  e)
                     switchType(geometry);
                 }
-
                 if (e.state === "complete") {
+                    const geometry =  e.graphic.geometry;
                     console.log("sketch on complete", e)
-                    // switchType(geometry);
-                    graphicsLayer.remove(graphicsLayer.graphics.getItemAt(0));
-                    
-                //   measurements.innerHTML = null;
+                    switchType(geometry);
+                    convertPolyline2Polygon(geometry);
+                    // graphicsLayer.remove(graphicsLayer.graphics.getItemAt(0));
+                    // measurements.innerHTML = null;
                 }
                 if (
                     e.toolEventInfo &&
                     (e.toolEventInfo.type === "scale-stop" ||
                     e.toolEventInfo.type === "reshape-stop" ||
                     e.toolEventInfo.type === "move-stop")
+                    
                 ) {
+                    console.log("sketch on rescale", e.graphics[0].geometry)
                     switchType(geometry);
                 }
-            });            
+            }); 
+
+            // sketch.on("update", (e) => {
+            //     console.log("sketch on update", e.graphics[0].geometry)
                 
+            //     // const geometry = e.graphic.geometry;
+            //     const geometry =  e.graphics[0].geometry;
+            //     if (e.state === "start") {
+            //         console.log("sketch on start",  geometry)
+            //         switchType(geometry);
+            //     }
+
+            //     if (e.state === "complete") {
+            //         console.log("sketch on complete", e)
+            //         // switchType(geometry);
+            //         graphicsLayer.remove(graphicsLayer.graphics.getItemAt(0));
+                    
+            //     //   measurements.innerHTML = null;
+            //     }
+            //     if (
+            //         e.toolEventInfo &&
+            //         (e.toolEventInfo.type === "scale-stop" ||
+            //         e.toolEventInfo.type === "reshape-stop" ||
+            //         e.toolEventInfo.type === "move-stop")
+                    
+            //     ) {
+            //         console.log("sketch on rescale", e.graphics[0].geometry)
+            //         switchType(geometry);
+            //     }
+            // });            
         }
 
     }, [sketchRef, map, mapView])
@@ -145,10 +227,7 @@ const MeasureComponentBeta = () => {
 
 
     return (
-        
-        <div id = "sketchy"  style = {{width:300, height:600}} ref={sketchRef} >
-
-        </div>
+        <div id = "measure-widget-sketch"  style = {{width:300, height:600}} ref={sketchRef} > </div>
     )      
 }
 
