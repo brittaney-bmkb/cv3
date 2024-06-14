7008a68008e9f440253d4064f89930275c2d935d
Brittaney Harkness <Brittaney.harkness@cookcountyil.gov>
2024-06-14 11:48:10 -0500

    fixed issue where mouseMove event attempted to style tool tip when select multiple parcels was not displayed

--- src/components/SelectMultipleParcels/SelectMultipleParcels.jsx
+++ src/components/SelectMultipleParcels/SelectMultipleParcels.jsx
@@ -226,27 +226,31 @@ const SelectMultipleParcels = () => {
 
 
     const handleMouseMove = (event) => {
-        const x = event.clientX 
-        const y = event.clientY
 
-        tooltipRef.current.style.left = x + 15 + 'px';
-        tooltipRef.current.style.top = y - 30 + 'px';
-        //tooltipRef.current.innerHTML = `select/deselect parcel`;
-        //tooltipRef.current.style.textWrap = 'wrap'
-        tooltipRef.current.style.backgroundColor = hexToRgba(theme.palette.secondary.light, .75)
-        //tooltipRef.current.style.opacity = "80%"
-        tooltipRef.current.padding = '10px'
-        tooltipRef.current.style.borderRadius = '15px'
-        tooltipRef.current.style.borderColor = 'transparent'
-        tooltipRef.current.style.width = '100px'
-        tooltipRef.current.style.display = 'flex';
-        tooltipRef.current.style.minHeight = "30px"
-        // Flexbox styles to center content vertically and horizontally
-        tooltipRef.current.style.textAlign = 'center';
-        tooltipRef.current.style.display = 'flex';
-        tooltipRef.current.style.alignItems = 'center';
-        tooltipRef.current.style.justifyContent = 'center';
-        tooltipRef.current.style.fontWeight = 600
+        if(tooltipRef.current){
+            const x = event.clientX 
+            const y = event.clientY
+    
+            tooltipRef.current.style.left = x + 15 + 'px';
+            tooltipRef.current.style.top = y - 30 + 'px';
+            //tooltipRef.current.innerHTML = `select/deselect parcel`;
+            //tooltipRef.current.style.textWrap = 'wrap'
+            tooltipRef.current.style.backgroundColor = hexToRgba(theme.palette.secondary.light, .75)
+            //tooltipRef.current.style.opacity = "80%"
+            tooltipRef.current.padding = '10px'
+            tooltipRef.current.style.borderRadius = '15px'
+            tooltipRef.current.style.borderColor = 'transparent'
+            tooltipRef.current.style.width = '100px'
+            tooltipRef.current.style.display = 'flex';
+            tooltipRef.current.style.minHeight = "30px"
+            // Flexbox styles to center content vertically and horizontally
+            tooltipRef.current.style.textAlign = 'center';
+            tooltipRef.current.style.display = 'flex';
+            tooltipRef.current.style.alignItems = 'center';
+            tooltipRef.current.style.justifyContent = 'center';
+            tooltipRef.current.style.fontWeight = 600
+    
+        }
 
         // Change the cursor to pointer
         //tooltipRef.current.style.cursor = 'pointer';
@@ -254,7 +258,10 @@ const SelectMultipleParcels = () => {
       };
 
       const handleMouseLeave = () => {
-        tooltipRef.current.style.display = 'none';
+        if(tooltipRef.current){
+            tooltipRef.current.style.display = 'none';
+        }
+        
 
         //element.style.cursor = 'default';
       };
