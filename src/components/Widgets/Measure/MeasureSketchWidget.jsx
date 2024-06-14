0b6af9b193a58ec86479b1730d0b99c2ccb86e22
Sigfrido Gomez (Geographic Information Systems) <Sigfrido.Gomez@cookcountyil.gov>
2024-06-14 12:45:20 -0500

    adding comments and cleaning up code.

--- src/components/Widgets/Measure/MeasureSketchWidget.jsx
+++ src/components/Widgets/Measure/MeasureSketchWidget.jsx
@@ -1,79 +1,30 @@
 
-import { Box, Button, Select, Typography, Stack, TextField, Divider, InputLabel , FormControl, NativeSelect   } from "@mui/material"
+import { Box, Button, Typography, Stack, Divider, InputLabel , FormControl, NativeSelect   } from "@mui/material"
 import UseAppContext from "../../../contexts/AppContext"
 import { useEffect, useRef, useState } from "react"
-import { ArcgisSketch } from "@arcgis/map-components-react"
 import GraphicsLayer from "@arcgis/core/layers/GraphicsLayer.js";
 import Graphic from "@arcgis/core/Graphic.js";
 import * as geometryEngine from "@arcgis/core/geometry/geometryEngine.js";
-import Sketch from "@arcgis/core/widgets/Sketch.js";
 import SketchViewModel from "@arcgis/core/widgets/Sketch/SketchViewModel.js";
-
-import StraightenOutlinedIcon from '@mui/icons-material/StraightenOutlined';
-import SquareFootOutlinedIcon from '@mui/icons-material/SquareFootOutlined';
-import DeleteSweepIcon from '@mui/icons-material/DeleteSweep';
-import SquareFootIcon from '@mui/icons-material/SquareFoot';
-import StyledButtonFilledPrimary from "../../Button/Button";
-import CalculateIcon from '@mui/icons-material/Calculate';
-import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
-import * as reactiveUtils from "@arcgis/core/core/reactiveUtils.js";
 import { CalciteIcon } from "@esri/calcite-components-react";
 import { theme } from "../../../theme";
 
-//// https://developers.arcgis.com/javascript/latest/tutorials/find-length-and-area/
-//// https://developers.arcgis.com/javascript/latest/tutorials/find-length-and-area/#add-an-event-listener
-//// https://developers.arcgis.com/javascript/latest/api-reference/esri-widgets-Sketch.html#update
-//// https://developers.arcgis.com/javascript/latest/api-reference/esri-widgets-Sketch.html#event-update
-//// https://developers.arcgis.com/javascript/latest/api-reference/esri-geometry-geometryEngine.html
-//// https://developers.arcgis.com/javascript/latest/api-reference/esri-widgets-Sketch.html#properties-summary
-//// https://developers.arcgis.com/javascript/latest/sample-code/sketch-geometries/
-//// https://developers.arcgis.com/javascript/latest/api-reference/esri-views-interactive-snapping-SnappingOptions.html
-//// https://developers.arcgis.com/javascript/latest/api-reference/esri-widgets-Sketch.html
-//// https://community.esri.com/t5/arcgis-javascript-maps-sdk-questions/how-to-convert-a-line-to-polygon-in-js-api-4-x/m-p/420334#M38612
-//// https://community.esri.com/t5/arcgis-javascript-maps-sdk-questions/getting-geodesic-area-ve/td-p/121084 // negative values 
-//// 
 
-
-//TODO Remove and draw should only after start of session - DONE 
-//TODO continue to test merge                     - DONE
-//TODO better layer out for buttons               - DONE 
-//TODO have abbr unit set                         - DONE 
-//TODO Inspect colors over aerial                 - DONE
-//TODO graphic style & add graphics to themes.js  - DONE
-//TODO typography                                 - DONE
-// TODO change abbrevbation to match drop down    - DONE
-
-//TODO have tool and layer close when tool exists - Done
-//TODO hittest - IP 
-
-
-//TODO Done button should only appear is graphic not complete
 
 const MeasureSketchWidget = () => {
     
-    const { mapView, map, translateText  } = UseAppContext()
+    const { mapView, translateText  } = UseAppContext()
     const graphicsLayer = useRef(null) // ESRI graphics
     let sketchVM = useRef(null) //ESRI sketchVM
     const [activeTool, setActiveTool] = useState(null) // active tool plyline or polygon 
     let [areaMeasurement, setAreaMeasurement] = useState(null) //polygon unit area
     let  [linearMeasurement, setLinearMeasurement] = useState(null) // line unit length 
-    
     let [userGeometry, setUserGeometry ] = useState(null); // user created geom in props
     let [selectedValue, setSelectedValue ] = useState(null); //drop down menu 
     let [unitAbbrev, setUnitAbbrev ] = useState(null) // unit abbreviation 
 
-    const handleChange = (e) => {
-        setSelectedValue(e.target.value)
-        unitMeasurementAbbrev(e.target.value)
-        
-        if (activeTool==='polygon'){
-            getArea(userGeometry, e.target.value)
-        } else{
-            getLength(userGeometry, e.target.value)
-        }
-    }
-
     const measurementUnitOptions = [
+        //TODO this object should be the main object
         { key: 'feet',              value: 'ft', label: translateText("feet")  },
         { key: 'yards',             value: 'yd', label: translateText("yards") },
         { key: 'miles',             value: 'mi', label: translateText("miles") },
@@ -102,7 +53,22 @@ const MeasureSketchWidget = () => {
         } 
     }
 
+    const handleChange = (e) => {
+        setSelectedValue(e.target.value)
+        unitMeasurementAbbrev(e.target.value)
+        
+        if (activeTool==='polygon'){
+            getArea(userGeometry, e.target.value)
+        } else{
+            getLength(userGeometry, e.target.value)
+        }
+    }
+
     const DropDownUnitMeasurement = () => {
+        // measurementUnitOptions should be the main object
+
+        // this function controls the drop down menu and handles the change for unit abbrv and 
+        // the unit calculation. 
         
         const linearUnitOptions = [
             {value: 'feet', label: translateText("feet") },
@@ -112,8 +78,6 @@ const MeasureSketchWidget = () => {
             {value: 'kilometers', label: translateText("kilometers") }
         ]
 
-        //https://developers.arcgis.com/javascript/latest/api-reference/esri-geometry-geometryEngine.html#AreaUnits
-        // "acres"|"ares"|"hectares"|"square-feet"|"square-meters"|"square-yards"|"square-kilometers"|"square-miles"
         const areaUnitOptions = [
             {value: 'acres', label: translateText("acres") },
             {value: 'square-feet', label: translateText("square-feet")  },
@@ -136,8 +100,6 @@ const MeasureSketchWidget = () => {
                         <Typography variant="subtitle2">{translateText("Unit of Measurement")}</Typography>
                     </InputLabel>
                         <NativeSelect
-                            // defaultValue={areaUnitOptions[0].value}
-                            // defaultValue={selectedValue}
                             inputProps={{
                             name: 'unitType',
                             id: 'unit-measure-select',
@@ -151,8 +113,6 @@ const MeasureSketchWidget = () => {
                                             {measureUnitOptions.label}
                                         </option>
                                     )) } */}
-                                
-                  
                             {activeTool ==="polygon" ? (
                                     areaUnitOptions.map((measureUnitOptions)  => (
                                         <option key={measureUnitOptions.value} value={measureUnitOptions.value}>
@@ -220,6 +180,7 @@ const MeasureSketchWidget = () => {
     }
     
     function switchType(geom) {
+        //switches between polyline and polygon and sets props as needed to reflect in widget
         switch (geom.type) {
             case "polygon":
                 setActiveTool(geom.type);
@@ -242,13 +203,15 @@ const MeasureSketchWidget = () => {
     }    
     
     const createGraphicLayer = async () => {
+        //creates the graphic needed to be added in the map. 
         graphicsLayer.current = new GraphicsLayer({
-            title:"measureGraphic"
+            title:"measureGraphic" // makes is searchable for WebMapComponentBeta to clear when widget not in use and for hittest to not occur
         })
         mapView.map.add(graphicsLayer.current)
     }
 
     const removeAllGraphics = async () => {
+        //removes all graphics and set props to null as if in a new session 
         graphicsLayer.current.removeAll();
         setActiveTool(null)       
         setAreaMeasurement(null)
@@ -259,42 +222,38 @@ const MeasureSketchWidget = () => {
     }
 
     const completeAllGraphics = async () => {
+        //completes all graphics with the done button
         sketchVM.current.complete();
-        // setActiveTool(null)       
-        // setAreaMeasurement(null)
-        // setLinearMeasurement(null)
-        // setSelectedValue(null)
-        // setUserGeometry(null)
     }
 
-    const checkDropDownAbbrev = (menuValue, abbrevValue) =>{
-        //levaing in this function for posterity but may not be needed. 
-        // console.log('Inside Console Log', abbrevValue)
-        // menuValue is key in object
-        // abbrevValue is value  in object. WE want this key in order to set it in the drop down. 
+    // const checkDropDownAbbrev = (menuValue, abbrevValue) =>{
+    //     //levaing in this function for posterity but may not be needed. 
+    //     // console.log('Inside Console Log', abbrevValue)
+    //     // menuValue is key in object
+    //     // abbrevValue is value  in object. WE want this key in order to set it in the drop down. 
 
-        // measurementUnitOptions.some(option => option.value === value && option.key === key);
-        // if (!abbrevValue){
-            const menuValueOption = measurementUnitOptions.find(option => menuValue === option.key );
-            const abbrevValueOption = measurementUnitOptions.find(option => abbrevValue === option.value );
+    //     // measurementUnitOptions.some(option => option.value === value && option.key === key);
+    //     // if (!abbrevValue){
+    //         const menuValueOption = measurementUnitOptions.find(option => menuValue === option.key );
+    //         const abbrevValueOption = measurementUnitOptions.find(option => abbrevValue === option.value );
 
-            if (menuValueOption.key === abbrevValueOption.key ){
-                setSelectedValue(abbrevValueOption.key)
-            }
+    //         if (menuValueOption.key === abbrevValueOption.key ){
+    //             setSelectedValue(abbrevValueOption.key)
+    //         }
     
-            console.log('menuValueOption',menuValueOption)
-            console.log('abbrevValueOption',abbrevValueOption)
+    //         console.log('menuValueOption',menuValueOption)
+    //         console.log('abbrevValueOption',abbrevValueOption)
 
-        // }
+    //     // }
 
 
-    }
+    // }
 
     const startMeasuring = async () => {
+        // heart of the widget, controls all creation and update of a polyline/polygon
+        // from here the majority of functions are altered. 
         if(!graphicsLayer.current){ await createGraphicLayer() }
         await initializeSketchVM()
-        // setActiveTool(tool)
-        // TODO IT takes two times for this to become active
 
         sketchVM.current.create("polyline");
         sketchVM.current.on("create", (e) => {
@@ -334,6 +293,7 @@ const MeasureSketchWidget = () => {
                 switchType(geometry);
             }
             if (e.state === "complete") {
+                //TODO NEED TO REMOVE AND TEST 
                 // console.log("sketch on complete", e)
                 // switchType(geometry);
                 // graphicsLayer.current.remove(graphicsLayer.current.graphics.getItemAt(0));
@@ -351,6 +311,8 @@ const MeasureSketchWidget = () => {
         });
     }    
     const checkLatLongArray = (geom) => {
+        //checks the first and last points in a line to see if they match. IF they match then the polyline should be
+        // converted to a polygon
         let polygonRings = geom.paths[0]
         let firstLat =polygonRings[0][0]
         let firstLon = polygonRings[0][1]
@@ -362,6 +324,8 @@ const MeasureSketchWidget = () => {
 
 
     const convertPolyline2Polygon = (geom) => {
+        //converts polyline to polygon and creates the graphic as needed. 
+        // colors need to be altered in theme js 
         let isPolygon = checkLatLongArray(geom)
         if (isPolygon){
             const polygon = {
@@ -391,6 +355,7 @@ const MeasureSketchWidget = () => {
     }
 
     const initializeSketchVM = async () =>{
+        //initializes the ESRI sketch View Model. 
         if(!sketchVM.current){
             sketchVM.current = new SketchViewModel({
                 view: mapView,
@@ -403,7 +368,7 @@ const MeasureSketchWidget = () => {
                 },                
                 defaultCreateOptions: { hasZ: false },
                 snappingOptions: { // autocasts to SnappingOptions()
-                    distance:8,
+                    distance:8, // snapping tolerance by pixels
                     enabled: true, // global snapping is turned on
                     featureEnabled: false,
                     selfEnabled: true,
@@ -421,7 +386,6 @@ const MeasureSketchWidget = () => {
 
             <Stack 
                 direction="row" spacing={2}
-                // divider={<Divider orientation="horizontal" flexItem />}
                 useFlexGap 
                 sx={{  
                     flexWrap: 'wrap',
@@ -444,41 +408,6 @@ const MeasureSketchWidget = () => {
                         </Typography>
                 </Button>
 
-                {/* { (activeTool) ? 
-
-                    <Button
-                        variant="contained"
-                        color="primary"
-                        sx={{textTransform:"none", 
-                        display:"flex", 
-                        flexDirection:"row", 
-                        columnGap:1,
-                        width: "40%",
-                    }}
-                        onClick={completeAllGraphics}>   
-                            <CalciteIcon icon="check-square"/>
-                            <Typography variant="body1">
-                                {translateText("Done")}
-                            </Typography>
-                    </Button> : '' }
-
-
-                { (activeTool) ? 
-                    <Button
-                        variant="contained"
-                        color="primary"
-                        sx={{textTransform:"none", 
-                        display:"flex", 
-                        flexDirection:"row", 
-                        columnGap:1,
-                        width: "40%",
-                    }}
-                        onClick={removeAllGraphics}>   
-                            <CalciteIcon icon="trash"/>
-                            <Typography variant="body1">
-                                {translateText("Remove")}
-                            </Typography>
-                    </Button> : '' } */}
             </Stack>
             <Divider />
             <DropDownUnitMeasurement/>
@@ -525,9 +454,6 @@ const MeasureSketchWidget = () => {
                 </Stack>
         : ''}
         </Box>        
-
-
-        
     )      
 }
 
