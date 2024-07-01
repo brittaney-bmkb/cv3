e51ae92cb5c022e1f8ebc9f318f9164d9a3d42a9
sigccil <sigfrido.gomez@cookcountyil.gov>
2024-07-01 14:17:33 -0500

    fixes the length and area when user hits done.

--- src/components/Widgets/Measure/MeasureSketchWidget.jsx
+++ src/components/Widgets/Measure/MeasureSketchWidget.jsx
@@ -71,8 +71,6 @@ const MeasureSketchWidget = () => {
 
 
     const handleChange = (e) => {
-        //TODO this breaks it
-        console.log("Value passed to area caluculations: ", e.target.value)
         
         if (activeTool==='polygon'){
             setSelectedValueArea(e.target.value)
@@ -138,7 +136,6 @@ const MeasureSketchWidget = () => {
     }
     
     const getArea = (polygon, unitType) => {
-        // console.log("Get area: ", polygon, unitType)
         const planarArea = geometryEngine.planarArea(polygon, unitType);
         const planarAreaPositive = getPositiveNumber(planarArea);
         setAreaMeasurement(planarAreaPositive) 
@@ -146,7 +143,6 @@ const MeasureSketchWidget = () => {
     }
     
     const getLength= (line, unitType) =>{
-        // console.log("Get length: ", line, unitType)
         const planarLength = geometryEngine.planarLength(line, unitType);
         const planarLengthPositive = getPositiveNumber(planarLength)
         setLinearMeasurement(planarLengthPositive) 
@@ -199,6 +195,8 @@ const MeasureSketchWidget = () => {
         //completes all graphics with the done button
         sketchVM.current.complete();
         setSketchState("complete")
+        // setActiveTool(null) 
+
     }
 
     useEffect(() => {
@@ -220,6 +218,8 @@ const MeasureSketchWidget = () => {
                         if (e.state === "complete") {
                             setUserGeometry(e.graphic.geometry) 
                             setSketchState("complete")
+                            switchType(geometry);
+
                         }
                         if (
                             e.toolEventInfo &&
