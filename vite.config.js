e59d220bc4a66e7f8bd1032f364b3d1dcb93401e
Brittaney Harkness <Brittaney.harkness@cookcountyil.gov>
2024-07-31 10:00:37 -0500

    updated base url

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/cookviewer-beta-parcel-locator-91"
+  base:"/cookviewer-beta-parcel-layer-source-and-locator"
 })
