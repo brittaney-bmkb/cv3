185162279a2d7f45823b401498cfb4f11103775d
brittaneyharkness <60359347+brittaneyharkness@users.noreply.github.com>
2024-08-09 12:10:44 -0500

    Update vite.config.js
    reverting base url back to cookviewer-beta
    
    Signed-off-by: brittaneyharkness <60359347+brittaneyharkness@users.noreply.github.com>

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/cookviewer-beta-parcel-layer-source-and-locator"
+  base:"/cookviewer-beta"
 })
