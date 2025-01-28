5529a84db0921f169fd4776bf49ec05e034d9268
sigccil <sigfrido.gomez@cookcountyil.gov>
2025-01-28 14:50:40 -0600

    adding the createSketchViewModel for handleStartNew

--- src/components/SelectMultipleParcels/SelectMultipleParcels.jsx
+++ src/components/SelectMultipleParcels/SelectMultipleParcels.jsx
@@ -193,6 +193,8 @@ const SelectMultipleParcels = () => {
 
         if(polygonGraphicsLayer.current && tool==="draw"){
             polygonGraphicsLayer.current.remove(sketchPolygon)
+            //start new sketch view model create session
+            await createSketchViewModel()
         }
 
         //if(tool==="click"){
@@ -203,7 +205,7 @@ const SelectMultipleParcels = () => {
             setSearchParams({})
             
         //}
-        
+        // handleSelectDraw
         
         setActionButtonsVisible(false)
     }
