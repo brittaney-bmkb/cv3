a51409855dbf216db322339b582632e77aa8d939
brittaneyharkness <brittaney.harkness@cookcountyil.gov>
2025-02-06 08:39:38 -0600

    adding language_codes variable to select localization codes for arcgis widgets and components

--- src/data/config.js
+++ src/data/config.js
@@ -53,6 +53,7 @@ export const config = {
     //LANGUAGES
     defaultLanguage: "english",
     languages:["english","spanish"],
+    lang_codes:{"english":"en","spanish":"es"},
     //APP TRANSLATION TEXT
     translation_text:"https://services2.arcgis.com/I5Or36sMcO7Y9vQ3/ArcGIS/rest/services/cookviewer_translated_text/FeatureServer/1",
     translation_text_help:"https://services2.arcgis.com/I5Or36sMcO7Y9vQ3/ArcGIS/rest/services/cookviewer_translated_text/FeatureServer/4",
