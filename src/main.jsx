ff643fd6c88c5e59cf173bd51f3ef288db790f86
Brittaney Harkness <Brittaney.harkness@cookcountyil.gov>
2025-02-21 13:21:13 -0600

    local path to point to root + assets

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import { ThemeProvider } from '@mui/material'
 import { theme } from './theme.js'
 import { setAssetPath } from '@esri/calcite-components/dist/components';
-setAssetPath(window.location.href + '/public/assets');
+setAssetPath(window.location.href + '/assets');
 //setAssetPath("https://unpkg.com/@esri/calcite-components/dist/calcite/assets");
 import { defineCustomElements } from "@arcgis/map-components/dist/loader";
 // Register custom elements
