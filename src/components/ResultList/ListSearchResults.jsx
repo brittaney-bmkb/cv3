0d3494a35278200b007cd84c175f52ffa268b154
Brittaney Harkness <Brittaney.harkness@cookcountyil.gov>
2025-03-05 15:53:52 -0600

    adding list selection options

--- src/components/ResultList/ListSearchResults.jsx
+++ src/components/ResultList/ListSearchResults.jsx
@@ -1,5 +1,5 @@
 
-import { CalciteList, CalciteListItem } from "@esri/calcite-components-react"
+import { CalciteLabel, CalciteList, CalciteListItem } from "@esri/calcite-components-react"
 import UseAppContext from "../../contexts/AppContext"
 
 const ListSearchResults = () => {
@@ -15,8 +15,22 @@ const ListSearchResults = () => {
                     key={feature.attributes['PIN14_dash']} 
                     label={feature.attributes['PIN14_dash']}
                     selectionAppearance="border"
-                    selectionMode="none"
-                    />
+                    selectionMode="multiple"
+                    open
+                    >
+                    <div slot="default">
+                        <CalciteLabel>
+                        
+                        </CalciteLabel>
+                        <CalciteLabel>
+                            {feature.attributes['street_address']}
+                        </CalciteLabel>
+                        <CalciteLabel>
+                            {feature.attributes['city_state_zip']}
+                        </CalciteLabel>
+                    </div>
+                        
+                    </CalciteListItem>
                 )
             })
         }
