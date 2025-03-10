f3c845292872371cd8bf783a74132b08d0e3e173
Brittaney Harkness <Brittaney.harkness@cookcountyil.gov>
2025-03-10 14:28:39 -0500

    added to does to property results - to update export and feedback actions

--- src/components/Panel/PanelSearchResults.jsx
+++ src/components/Panel/PanelSearchResults.jsx
@@ -2,12 +2,6 @@ import {
     CalciteAction, 
     CalciteActionBar, 
     CalciteBlock, 
-    CalciteBlockSection,
-    CalciteList,
-    CalciteListItem,
-    CalciteListItemGroup,
-    CalciteLink,
-    CalciteLabel,
     CalcitePanel, 
 } from "@esri/calcite-components-react"
 import UseAppContext from "../../contexts/AppContext";
@@ -15,12 +9,24 @@ import ListSearchResults from "../ResultList/ListSearchResults";
 import { config } from "../../data/config";
 
 
+//TODO - Update Export dialog and add trigger to export action
+//TODO - Update Feedback dialog and add trigger to feedback action
+
 const PanelSearchResults = () => {
 
-    const { searchTerm, searchBufferGeometry, searchFeatures, translateText, searchResultsPanelClosed, setSearchResultsPanel, setInfoPanel } = UseAppContext()
+    const { 
+        searchTerm, 
+        searchBufferGeometry, 
+        searchFeatures, 
+        translateText, 
+        searchResultsPanelClosed, 
+        setSearchResultsPanel, 
+        clearResults,
+        //setOpenExportDialog
+    } = UseAppContext()
 
     return (
-                <CalcitePanel 
+            <CalcitePanel 
                 id="search-results-panel" 
                 closed={searchResultsPanelClosed} 
                 closable 
@@ -35,37 +41,36 @@ const PanelSearchResults = () => {
                 >   
                     {/* SEARCH RESULT ACTIONS */}
                     <CalciteActionBar slot="action-bar" layout="horizontal" expandDisabled> 
-                        <CalciteAction text="clear" icon="reset" disabled={searchFeatures ? false : true} textEnabled scale="s"></CalciteAction>
-                        <CalciteAction text="export" icon="export" disabled={searchFeatures ? false : true} textEnabled scale="s"></CalciteAction>
-                        <CalciteAction text="feedback" icon="speech-bubble-exclamation" disabled={searchFeatures ? false : true} textEnabled scale="s"/>
+                        <CalciteAction 
+                            text="clear" 
+                            icon="reset" 
+                            disabled={searchFeatures ? false : true} 
+                            textEnabled 
+                            scale="s"
+                            onClick={clearResults}
+                        ></CalciteAction>
+                        <CalciteAction 
+                            text="export" 
+                            icon="export" 
+                            disabled={searchFeatures ? false : true} 
+                            textEnabled 
+                            scale="s"
+                           //onClick={() => {setOpenExportDialog(true)}}
+                        ></CalciteAction>
+                        <CalciteAction 
+                            text="feedback" 
+                            icon="speech-bubble-exclamation" 
+                            disabled={searchFeatures ? false : true} 
+                            textEnabled 
+                            scale="s"
+                        />
                     </CalciteActionBar>
         
                     <CalciteBlock open collapsible={false}>
                         {/* SEARCH RESULTS LABEL*/}
-                        {searchFeatures ? <ListSearchResults/> : `Search for new property`} 
+                        {searchFeatures ? <ListSearchResults/> : translateText(`Search for new property`)} 
                     </CalciteBlock>
-                </CalcitePanel>
-        // <CalcitePanel 
-        // id="search-results-panel" 
-        // closed={searchResultsPanelClosed} 
-        // closeable
-        // class='panel-start' 
-        // heading={searchFeatures ? `${searchFeatures.length} Search Results` : `Search for new property`}
-        // description= {searchBufferGeometry ? `${translateText("Property results include parcels within")} ${config.buffer_distance} ${translateText(config.buffer_unit)} ${translateText("of")} ${searchTerm}`: null}
-        // overlayPositioning="fixed" 
-        // calcitePanelClose={() => {
-        //     setSearchResultsPanel(true)
-        // }}
-        // style={{display: searchResultsPanelClosed ? 'none': 'flex'}}
-        // >
-
-
-        //     <CalciteBlock open={true} >
-        //         {/* SEARCH RESULTS */}
-        //         <ListSearchResults/>
-                
-        //     </CalciteBlock>
-        // </CalcitePanel>
+            </CalcitePanel>
     )
 }
 
