14f06b3ec486ae7a0b434fe13600cd5a10002799
Brittaney Harkness <Brittaney.harkness@cookcountyil.gov>
2025-03-18 12:07:30 -0500

    feedback links added to config instead of hardcoded into feedback component

--- src/data/config.js
+++ src/data/config.js
@@ -35,6 +35,10 @@ export const config = {
     secondary_color:'',
     //PRINT OPTIONS
     print_orientation_options : ['Landscape', 'Portrait'],
+    //FEEDBACK URLS
+    feedback_general:'//survey123.arcgis.com/share/ba8f1d610701420abc33612ef1d3378a?hide=navbar,footer',
+    feedback_extended:'//survey123.arcgis.com/share/640dd8fd0d064eb880b65cc3d238f87f?hide=navbar,footer',
+    feedback_search:'//survey123.arcgis.com/share/6c24e84d3ac24024a311b7e81045c382?hide=navbar,footer',
     //DATA DICTIONARY SERVICE
     data_dictionary: "https://services2.arcgis.com/I5Or36sMcO7Y9vQ3/arcgis/rest/services/cookviewer_data_dictionary/FeatureServer/0",
     // TARGET LAYER CONFIGURATION: Specify the name of the target layer and the fields to display in results.
