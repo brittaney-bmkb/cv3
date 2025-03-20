cfc659a6a1d242c53b770800b29e972e37904aa0
brittaneyharkness <brittaney.harkness@cookcountyil.gov>
2025-03-20 12:31:46 -0500

    Vertical menu for mobile view

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,11 @@
-import { CalciteAction, CalciteActionBar, CalciteFlow, CalciteFlowItem, CalciteLabel, CalciteMenu, CalciteMenuItem, CalciteNavigation, CalciteNavigationLogo } from "@esri/calcite-components-react"
+import { CalciteNavigation, CalciteNavigationLogo } from "@esri/calcite-components-react"
 import { config } from "../../data/config"
 import SearchBarComponent from "../SearchBar/SearchBarComponent"
-import HeaderMenu from "./HeaderMenu"
+import HeaderMenu, { HeaderMenuMobile } from "./HeaderMenu"
+import UseAppContext from "../../contexts/AppContext"
+
 import "@esri/calcite-components/components/calcite-navigation"
 import "@esri/calcite-components/components/calcite-navigation-logo"
-import "@esri/calcite-components/components/calcite-menu"
-import "@esri/calcite-components/components/calcite-menu-item"
-import "@esri/calcite-components/components/calcite-flow-item"
-import "@esri/calcite-components/components/calcite-flow"
-import UseAppContext from "../../contexts/AppContext"
-import * as intl from "@arcgis/core/intl.js";
-import { useRef, useState } from "react"
-
 
 const Header = () => {
 
@@ -45,7 +39,7 @@ const Header = () => {
 
             {
                 isMobile ?
-                null:
+                <HeaderMenuMobile/>:
                 <HeaderMenu/>
             }
               
--- src/components/Header/HeaderMenu.jsx
+++ src/components/Header/HeaderMenu.jsx
@@ -1,6 +1,15 @@
-import { CalciteMenu, CalciteMenuItem } from "@esri/calcite-components-react"
+import { CalciteButton, CalciteFlow, CalciteFlowItem, CalciteMenu, CalciteMenuItem } from "@esri/calcite-components-react"
 import { config } from "../../data/config"
 import UseAppContext from "../../contexts/AppContext"
+import * as intl from "@arcgis/core/intl.js";
+
+
+import "@esri/calcite-components/components/calcite-menu"
+import "@esri/calcite-components/components/calcite-menu-item"
+import "@esri/calcite-components/components/calcite-flow-item"
+import "@esri/calcite-components/components/calcite-flow"
+import { FlowItem } from "@esri/calcite-components/components/calcite-flow-item";
+import { useRef, useState } from "react";
 
 function titleCase(s) {
     return s.toLowerCase()
@@ -13,11 +22,11 @@ function titleCase(s) {
 const menuItems = {
     Help: {
         icon: "question",
-        subMenuItems: {},
+        subMenuItems: null,
     },
     Feedback:{
         icon:"speech-bubble-exclamation",
-        subMenuItems: {},
+        subMenuItems: null,
     },
     Translate: {
         icon: "language-translate",
@@ -66,7 +75,7 @@ const HeaderMenu = () => {
                             }}
                         >
                             {
-                                Object.keys(menuItems[menuItem].subMenuItems).map(subMenuItem => {
+                                menuItems[menuItem].subMenuItems ? Object.keys(menuItems[menuItem].subMenuItems).map(subMenuItem => {
                                     return(
                                             <CalciteMenuItem 
                                                 slot="submenu-item" 
@@ -79,6 +88,7 @@ const HeaderMenu = () => {
                                                 }}/>
                                     )
                                 })
+                                : null
                             }
                         </CalciteMenuItem>
                     )
@@ -92,6 +102,113 @@ const HeaderMenu = () => {
 
 export default HeaderMenu
 
-const HeaderMenuMobile = (menuItems) => {
+export const HeaderMenuMobile = () => {
 
+    const {
+        translateText,
+        setLanguage,
+        setFeedbackDialog,
+    } = UseAppContext()
+
+    const [ menuOpen, setMenuOpen ] = useState(false)
+    const flowRef = useRef(null)
+    const [flowItems, setFlowItems] = useState([]);
+
+    const handleClick = (language) => {
+        console.log("selected language")
+        setLanguage(language)
+
+        //reference: https://developers.arcgis.com/javascript/latest/localization/
+        let locale_code = config.language_codes[language]
+        console.log("setting locale code to: ", locale_code)
+        intl.setLocale(locale_code)
+        console.log("locale code to: ", intl.getLocale())
+    }
+
+    const createFlowItem = (title) => {
+        const newFlowItem = (
+            <CalciteFlowItem 
+                key={title} 
+                heading={title} 
+                onCalciteFlowItemBack={() => removeFlowItem(title)}
+                selected
+            >
+                <CalciteMenu layout="vertical">
+                    {Object.keys(menuItems[title].subMenuItems).map(submenuItem => {
+                        return(
+                            <CalciteMenuItem 
+                            key={`${submenuItem}`} 
+                            text={`${submenuItem}`} 
+                        />
+                        )
+                    })}
+                </CalciteMenu>
+            </CalciteFlowItem>
+        );
+
+        // Add the new flow item while deselecting others
+        setFlowItems((prev) => [...prev.map(item => ({ ...item, selected: false })), newFlowItem]);
+    };
+
+    const removeFlowItem = (title) => {
+        setFlowItems((prev) => prev.filter(item => item.key !== title));
+    };
+
+
+    return(
+        <>
+        <CalciteButton 
+        slot="content-end" 
+        iconStart="hamburger"
+        className="hyperlink-button"
+        width="full"
+        scale="l"
+        onClick={() => {setMenuOpen(!menuOpen)}}
+        /> 
+
+        {
+        menuOpen ? 
+        <CalciteFlow slot="navigation-tertiary" className='overlay-menu'>
+            <CalciteFlowItem heading="Menu" ref={flowRef}>
+                <CalciteMenu
+                layout="vertical"
+                >
+                    {Object.keys(menuItems).map(menuItem => {
+
+                        if(menuItems[menuItem].subMenuItems){
+                            return(
+                                <></>
+                            )                         
+                        }
+                        else{
+                            return(
+                                <CalciteMenuItem
+                                key={menuItem}
+                                text={translateText(menuItem)} 
+                                iconStart={menuItems[menuItem].icon}
+                                label={menuItem}
+                                text-enabled
+                                onCalciteMenuItemSelect={() => {
+                                    if(menuItem === 'Feedback'){
+                                        setFeedbackDialog(true, 'extended')
+                                    }
+                                    
+                                    else{
+                                        setMenuOpen(false)
+                                    }
+                                    
+                                }}></CalciteMenuItem>
+                            )
+                        }
+
+                        
+                    })}
+                </CalciteMenu>
+            </CalciteFlowItem>
+            {flowItems}
+        </CalciteFlow>
+        : null }
+        </>
+        
+    )
 }
