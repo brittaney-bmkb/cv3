32360b39efa8112615ec4ff1ac4f946c58937c4f
Brittaney Harkness <Brittaney.harkness@cookcountyil.gov>
2025-05-07 16:22:23 -0500

    update base url

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/cookviewer",
+  base:"/cookviewer-tour",
   publicDir: 'public'
 })
