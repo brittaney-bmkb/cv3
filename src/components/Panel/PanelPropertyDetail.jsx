eb033fcf8b157e963bcf95cc73680a923b693470
brittaneyharkness <brittaney.harkness@cookcountyil.gov>
2025-06-16 08:52:29 -0500

    added translation function to button text

--- src/components/Panel/PanelPropertyDetail.jsx
+++ src/components/Panel/PanelPropertyDetail.jsx
@@ -470,20 +470,20 @@ const PanelPropertyDetail = () => {
                                                                 return(
                                                                     <CalciteListItem
                                                                     key={data?.attributes['field']}
-                                                                    label={calculatedValues[data?.attributes['field']]['label']}
+                                                                    label={translateText(calculatedValues[data?.attributes['field']]['label'])}
                                                                     description={translateText(calculatedValues[data?.attributes['field']]['description'])}
                                                                     open
                                                                     >
                                                                         <div slot="content">
                                                                             <CalciteButton 
                                                                             className='hyperlink-button' 
-                                                                            label={calculatedValues[data?.attributes['field']]['label']}
+                                                                            label={translateText(calculatedValues[data?.attributes['field']]['label'])}
                                                                             iconStart="launch"
                                                                             href={hyperlink} 
                                                                             target="_blank"
                                                                             scale='m'
                                                                             >
-                                                                                {calculatedValues[data?.attributes['field']]['label']}
+                                                                                {translateText(calculatedValues[data?.attributes['field']]['label'])}
                                                                             </CalciteButton>
                                                                             <CalciteLabel scale='s' className='description'>
                                                                                 {translateText(calculatedValues[data?.attributes['field']]['description'])}
